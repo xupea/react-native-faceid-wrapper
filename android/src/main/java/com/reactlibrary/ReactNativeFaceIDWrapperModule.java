11188cc42d6aa1dfae1d4dabb5578962d4e4cdd9
peter <peter@JAVIS.local>
2018-09-28 17:03:05 +0800

    support face id detect

--- android/src/main/java/com/reactlibrary/ReactNativeFaceIDWrapperModule.java
+++ android/src/main/java/com/reactlibrary/ReactNativeFaceIDWrapperModule.java
@@ -1,23 +1,32 @@
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
+import com.facebook.react.bridge.WritableMap;
 import com.megvii.faceidiol.sdk.manager.IDCardDetectListener;
 import com.megvii.faceidiol.sdk.manager.IDCardManager;
 import com.megvii.faceidiol.sdk.manager.UserDetectConfig;
+
+import com.megvii.faceid.zzplatform.sdk.listener.FaceIdDetectListener;
+import com.megvii.faceid.zzplatform.sdk.listener.FaceIdInitListener;
+import com.megvii.faceid.zzplatform.sdk.manager.FaceIdManager;
 
 public class ReactNativeFaceIDWrapperModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
   private UserDetectConfig config;
+  private String bizToken;
+  private String mSign;
 
   public ReactNativeFaceIDWrapperModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    FaceIdManager.getInstance(reactContext).setFaceIdInitListener(mInitListener);
+    FaceIdManager.getInstance(reactContext).setFaceIdDetectListener(mDetectListener);
   }
 
   @Override
@@ -26,7 +35,7 @@ public class ReactNativeFaceIDWrapperModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void startDetectID(String apiKey, String secret, Promise promise) {
+  public void startDetectID(String apiKey, String secret) {
     config = new UserDetectConfig();
     beginDetect(apiKey, secret);
   }
@@ -58,4 +67,53 @@ public class ReactNativeFaceIDWrapperModule extends ReactContextBaseJavaModule {
       }
     }).start();
   }
+
+  @ReactMethod
+  public void startDetectFace(String bizToken, Promise promise) {
+    this.bizToken = bizToken;
+    detectPromise = promise;
+    enterNextPage();
+  }
+
+  @ReactMethod
+  public void getAppSign(String apiKey, String secret, Promise promise) {
+    long currtTime = System.currentTimeMillis() / 1000;
+    long expireTime = System.currentTimeMillis() / 1000 + 60 * 60 * 24;
+    mSign = GenerateSign.appSign(apiKey, secret, currtTime, expireTime).replaceAll("[\\s*\t\n\r]", "");
+    WritableMap map = Arguments.createMap();
+    map.putString("sign", mSign);
+    promise.resolve(map);
+  }
+
+  private void enterNextPage(){
+    FaceIdManager.getInstance(reactContext).init(bizToken);
+  }
+
+  private FaceIdInitListener mInitListener = new FaceIdInitListener() {
+    @Override
+    public void onSuccess() {
+      FaceIdManager.getInstance(reactContext).startDetect();
+    }
+
+    @Override
+    public void onFailed(int code, String msg) {
+    }
+  };
+
+  private Promise detectPromise;
+
+  private FaceIdDetectListener mDetectListener = new FaceIdDetectListener() {
+    @Override
+    public void onSuccess(int code, String msg) {
+      WritableMap map = Arguments.createMap();
+      map.putInt("code", code);
+      map.putString("msg", msg);
+      detectPromise.resolve(map);
+    }
+
+    @Override
+    public void onFailed(int code, String msg) {
+      detectPromise.reject(String.format("%d", code), msg);
+    }
+  };
 }
